1e09b81bb62f404dae6470c5c43c0ca73c4958c2
Sairam Harikrishnan <sai_hari98@hotmail.com>
2024-02-10 18:54:42 -0600

    added java implementation for yq custom

---
+++ yq-custom/YAML.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class YAML{
+
+    private String path;
+    private File file;
+    public YAML(String path) throws Exception{
+        this.file = new File(path);
+        if(!this.file.exists()){
+            throw new Exception("File not found");
+        }
+        this.path = path;
+    }
+
+    public void printFile() throws IOException{
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+        while(bufferedReader.ready()){
+            System.out.println(bufferedReader.readLine());
+        }
+    }
+}
---
+++ yq-custom/Yq.java
@@ -0,0 +1,6 @@
+public class Yq{
+    public static void main(String[] args) {
+        YAML yamlFile = new YAML(args[0]);
+        yamlFile.printFile();
+    }
+}
